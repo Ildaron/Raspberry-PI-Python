dcc4f187e9f9980ef4ef2dd27278fca0780fe29e
Ildar Rakhmatulin <rakhmatulin@ad.jmc.asia>
2020-03-12 09:27:37 +0900

    release v1

---
+++ 1.UART_Raspberry_PI_Python.py
@@ -0,0 +1,17 @@
+# надо тока один  их портов отключить в конфишупраайцмм
+# https://www.electronicwings.com/raspberry-pi/raspberry-pi-uart-communication-using-python-and-c
+
+#https://pimylifeup.com/raspberry-pi-serial/
+
+import serial
+from time import sleep
+
+ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
+
