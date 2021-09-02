0f4b4069fd4e81c0a311e4a4b2f51ce08b7ad3a9
Ildaron <de.ikar@yandex.ru>
2021-09-02 13:53:23 +0900

    Update 6.DIOD.py

--- 6.DIOD.py
+++ 6.DIOD.py
@@ -1,1 +1,11 @@
+from RPi import GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+
+while True:
+  GPIO.output(23, True)
+  sleep(0.5)
+  GPIO.output(23, False)
+  sleep(0.5)
 
